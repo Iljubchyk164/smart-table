161aec814a6bf55c0e053a07255fd8e8492d18b0
Iljubchyk <IljubchykCom3@yandex.by>
2026-05-24 01:46:51 +0300

    feat: pagination

--- src/components/pagination.js
+++ src/components/pagination.js
@@ -1,18 +1,40 @@
 
-export function initSearching(searchField) {
-    // @todo: #5.1 — настроить компаратор
+import { getPages } from "../lib/utils.js";
 
+export const initPagination = ({ pages, fromRow, toRow, totalRows }, createPage) => {
+    const pageTemplate = pages.firstElementChild.cloneNode(true);    // в качестве шаблона берём первый элемент из контейнера со страницами
+    pages.firstElementChild.remove();   
+    let pageCount;
 
-
-    /*const compareDate = createComparison(['skipNonExistentSourceFields', 'skipEmptyTargetValues', 'stringIncludes', 'caseInsensitiveStringIncludes', 'stringExactMatch']);
-    const compareCustomer = createComparison(['skipNonExistentSourceFields', 'skipEmptyTargetValues', 'stringIncludes', 'caseInsensitiveStringIncludes', 'stringExactMatch']);
-    const compareSeller = createComparison(['skipNonExistentSourceFields', 'skipEmptyTargetValues', 'stringIncludes', 'caseInsensitiveStringIncludes', 'stringExactMatch']);
-    const compareTotal = createComparison(['skipNonExistentSourceFields', 'skipEmptyTargetValues', 'stringExactMatch', 'numericTolerance']);*/
-
-
-    return (query, state, action) => { // result заменили на query
-        return state[searchField] ? Object.assign({}, query, { // проверяем, что в поле поиска было что-то введено
-            search: state[searchField] // устанавливаем в query параметр
-        }) : query; // если поле с поиском пустое, просто возвращаем query без изменений
+    const applyPagination = (query, state, action) => {
+        const limit = state.rowsPerPage;
+        let page = state.page;
+        if (action) switch (action.name) {
+            case 'prev': page = Math.max(1, page - 1); break;            // переход на предыдущую страницу
+            case 'next': page = Math.min(pageCount, page + 1); break;    // переход на следующую страницу
+            case 'first': page = 1; break;                                // переход на первую страницу
+            case 'last': page = pageCount; break;                        // переход на последнюю страницу
+        }
+        return Object.assign({}, query, { // добавим параметры к query, но не изменяем исходный объект
+            limit,
+            page
+        });
     }
-}
+
+    const updatePagination = (total, { page, limit }) => {
+        pageCount = Math.ceil(total / limit);
+        const visiblePages = getPages(page, pageCount, 5);                // Получим массив страниц, которые нужно показать, выводим только 5 страниц
+        pages.replaceChildren(...visiblePages.map(pageNumber => {        // перебираем их и создаём для них кнопку
+            const el = pageTemplate.cloneNode(true);                    // клонируем шаблон, который запомнили ранее
+            return createPage(el, pageNumber, pageNumber === page);        // вызываем колбэк из настроек, чтобы заполнить кнопку данными
+        }))
+        fromRow.textContent = (page - 1) * limit + 1;                    // С какой строки выводим
+        toRow.textContent = Math.min((page * limit), total);    // До какой строки выводим, если это последняя страница, то отображаем оставшееся количество
+        totalRows.textContent = total;                                // Сколько всего строк выводим на всех страницах вместе (после фильтрации будет меньше)
+        
+    }
+        return {
+            updatePagination,
+            applyPagination
+        }
+    }
